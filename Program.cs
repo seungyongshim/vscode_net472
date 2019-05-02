d3fe104b71a39398f8ecdd5ef5902b079a77bdd6
Chris Shim <syshim@me.com>
2019-05-02 22:29:51 +0900

    - 소스코드 등록

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Mat("e:/1.png");
+        }
+    }
+}
